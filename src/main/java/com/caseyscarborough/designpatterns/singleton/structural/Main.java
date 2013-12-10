7c250e8490ca268a5ca57646a3ddea82747e9a8f
Casey Scarborough <caseyscarborough@gmail.com>
2013-12-09 21:12:04 -0500

    Add structural Singleton pattern.

--- src/main/java/com/caseyscarborough/designpatterns/singleton/structural/Main.java
+++ src/main/java/com/caseyscarborough/designpatterns/singleton/structural/Main.java
@@ -1,7 +1,14 @@
 package com.caseyscarborough.designpatterns.singleton.structural;
 
-/**
- * Created by Casey on 09/12/2013.
- */
 public class Main {
+
+  public static void main(String[] args) {
+    Singleton s1 = Singleton.getInstance();
+    Singleton s2 = Singleton.getInstance();
+
+    if (s1 == s2) {
+      System.out.println("Objects are the same instance.");
+    }
+  }
+
 }
--- src/main/java/com/caseyscarborough/designpatterns/singleton/structural/Singleton.java
+++ src/main/java/com/caseyscarborough/designpatterns/singleton/structural/Singleton.java
@@ -1,7 +1,26 @@
 package com.caseyscarborough.designpatterns.singleton.structural;
 
 /**
- * Created by Casey on 09/12/2013.
+ * Singleton Class
+ *
+ * This class is the only component of the Singleton design pattern.
+ * The point of this design pattern is to ensure a class has only one
+ * instance and to provide a global point of access to it.
  */
 public class Singleton {
+
+  private static Singleton instance;
+
+  // Singleton uses a private or protected constructor.
+  private Singleton() { }
+
+  public static Singleton getInstance() {
+    // Lazily instantiate our Singleton instance.
+    if (instance == null) {
+      instance = new Singleton();
+    }
+
+    return instance;
+  }
+
 }
